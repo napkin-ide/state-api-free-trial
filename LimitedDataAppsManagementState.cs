b88fd3f1392decf437e0d8c1baddfefd6ba4ca6f
ttrichar <trevor.richardson@fathym.com>
2020-03-27 15:40:14 -0600

    Initial States and Harnesses seeded

---
+++ LimitedDataAppsManagementState.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Fathym;
+using LCU.Presentation.State.ReqRes;
+using LCU.StateAPI.Utilities;
+using LCU.StateAPI;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace LCU.State.API.NapkinIDE.NapkinIDE.LimitedTrial
+{
+    [Serializable]
+    [DataContract]
+    public class LimitedDataAppsManagementState
+    {
+        #region Constants
+        public const string HUB_NAME = "limiteddataappsmanagement";
+        #endregion
+        
+        [DataMember]
+        public virtual bool Loading { get; set; }
+    }
+}
